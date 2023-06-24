5b22bc0daddaab94f0065264d65b58fd2660ccfe
Germán <GassetGerman@gmail.com>
2023-06-24 21:28:04 +0200

    initial commit (starting second algorithm)

---
+++ Algorithms.h
@@ -0,0 +1,75 @@
+#include <string>
+
+#pragma once
+class Algorithms
+{
+public:
+	static int atoi(char* input)
+	{
+		std::string formatException = "Format Exception";
+
+		int output = 0;
+
+		// Count not null characters
+		int input_length = 0;
+		for (int i = 0; input[i] != '\000'; i++, input_length++) {}
+
+
+		char currentC;
+		int digit_count = input_length;
+		int sign_multiplier = 1;
+		for (int i = 0; (currentC = input[i]) != '\000'; i++)
+		{
+			char currentC = input[i];
+
+			if ((currentC > '9' || currentC < '0') && (currentC != '+' && currentC != '-'))
+				throw formatException;
+
+			if (i > 0 && (currentC == '+' || currentC == '-'))
+				throw formatException;
+
+			int is_sign = currentC == '+' || currentC == '-';
+
+			// If the number is a negative sign store it for later processing
+			int is_negative_sign = currentC == '-';
+			sign_multiplier -= 2 * is_negative_sign;
+
+			// If the current character is a sign no more processing is required
+			if (is_sign)
+				continue;
+
+			// Process number to add it to output
+			int unprocessed_number = currentC - '0';
+			int processed_number = unprocessed_number * static_cast<int>(pow(10, digit_count - i - 1));
+			output += processed_number;
+		}
+
+		output *= sign_multiplier;
+		return output;
+	}
+
+	static bool ValidParenthesis(char* input)
+	{
+		int input_length = 0;
+		for (int i = 0; input[i] != '\000'; i++, input_length++) {  }
+
+		/*
+			Parenthesis positions:
+				int*** (length = input_length)
+					int** (length = 2):
+						int* (length = 3):
+							'(' at index 0
+							'[' at index 1
+							'{' at index 2
+
+						int* (length = 3): 
+							')' at index 0
+							']' at index 1
+							'}' at index 2
+		*/
+		int*** parenthesisPositions = (int***)malloc(sizeof(int**) * input_length);
+
+
+	}
+};
+
---
+++ C++Algorithms.cpp
@@ -0,0 +1,59 @@
+// C++Algorithms.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include "Algorithms.h"
+
+void stringToInteger();
+
+
+int main()
+{
+    char* input = (char*)malloc(sizeof(char) * 20);
+    std::string menu = "Enter a command mentioned below to execute a home-made program\n\"cancel\" to exit the program\n\"atoi\" basically a string to integer";
+    while (true)
+    {
+        std::cout << menu << "\n";
+        std::cin >> input;
+        if (!std::strcmp(input, "cancel"))
+            break;
+        if (!std::strcmp(input, "atoi"))
+            stringToInteger();
+    }
+    free(input);
+}
+
+void stringToInteger()
+{
+    char* input = (char*)malloc(sizeof(char) * 1000);
+    while (true)
+    {
+        std::cout << "Enter a number or type \"cancel\" to go back to the menu without memory leaks\n";
+        _Notnull_ std::cin >> input;
+        std::cout << "\n";
+        if (!std::strcmp(input, "cancel"))
+            break;
+        try
+        {
+            std::string integerResult = std::to_string(Algorithms::atoi(input));
+            std::cout << "Parsed number: " + integerResult + "\n";
+        }
+        catch (const std::string& err)
+        {
+            std::cout << "Error while parsing the number";
+        }
+        std::cout << "\n\n";
+    }
+    free(input);
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
